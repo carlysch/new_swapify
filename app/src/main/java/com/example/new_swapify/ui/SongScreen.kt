03d4131704577dd1ce7b4baa14562e77958834c3
Carly Scheuerman <90992086+carlysch@users.noreply.github.com>
2023-06-07 16:03:09 -0700

    Theme added

--- app/src/main/java/com/example/new_swapify/ui/SongScreen.kt
+++ app/src/main/java/com/example/new_swapify/ui/SongScreen.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 //import androidx.compose.material.Icon
 //import androidx.compose.material.MaterialTheme
 //import androidx.compose.material.Text
--- app/src/main/java/com/example/new_swapify/ui/theme/Type.kt
+++ app/src/main/java/com/example/new_swapify/ui/theme/Type.kt
@@ -1,34 +1,69 @@
 package com.example.new_swapify.ui.theme
-
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.new_swapify.R
+
+private val Inconsolata = FontFamily(
+    Font(R.font.inconsolata_regular),
+    Font(R.font.inconsolata_medium, FontWeight.W500),
+    Font(R.font.inconsolata_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+val typography = Typography(
+    h4 = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W600,
+        fontSize = 30.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    h5 = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = Inconsolata,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    button = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = Inconsolata,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
+
     )
-    */
 )
