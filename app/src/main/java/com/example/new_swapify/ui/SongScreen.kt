832eb3abc185912696ee291150ffa0e881a4b709
Carly Scheuerman <90992086+carlysch@users.noreply.github.com>
2023-06-07 13:47:24 -0700

    reads from firebase

---
+++ app/src/main/java/com/example/new_swapify/ui/SongScreen.kt
@@ -0,0 +1,47 @@
+package com.example.new_swapify.ui
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+//import androidx.compose.material.Icon
+//import androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.new_swapify.R
+import com.example.new_swapify.ui.theme.New_swapifyTheme
+
+@Composable
+fun SongScreen(
+    songViewModel: SongViewModel = viewModel()
+) {
+    val userList = songViewModel.data.value.toString()
+      Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()) {
+          Text(text = userList)
+      }
+}
+@Preview(showBackground = true)
+@Composable
+fun GmeScreenPreview() {
+    New_swapifyTheme {
+        SongScreen()
+    }
+}
